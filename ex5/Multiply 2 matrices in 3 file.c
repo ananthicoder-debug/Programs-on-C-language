f40879bbba7d7826a9fcfa74b14cdc25e6e507ae
Anantha valli U <ananthigomathi202006@gmail.com>
2026-04-19 11:46:50 +0530

    Add matrix multiplication function prototype

---
+++ ex5/Multiply 2 matrices in 3 file.c
@@ -0,0 +1,5 @@
+//head
+#include<stdio.h>
+void matmul(int r1,int c1,int c2,int[][c1],int[][c2],int[][c2]);
+void add(int r1,int c1,int[][c1],int[][c1],int[][c1]);
+//void display(int,int,int,int[][c1],int[][c2]);
