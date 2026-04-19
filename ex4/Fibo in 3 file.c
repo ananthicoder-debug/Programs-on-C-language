699121c082f481997d1407c45c8bc96a8d57ce37
Anantha valli U <ananthigomathi202006@gmail.com>
2026-04-19 11:30:29 +0530

    Remove unnecessary comment from Fibo in 3 file.c

---
+++ ex4/Fibo in 3 file.c
@@ -0,0 +1,29 @@
+//head
+#include"head.h"
+int fibbo(int n)
+{
+   if(n<=1)
+      return n;
+   else
+      return fibbo(n-1)+fibbo(n-2);
+}
+//imp
+#include"head.h"
+int fibbo(int n)
+{
+   if(n<=1)
+      return n;
+   else
+      return fibbo(n-1)+fibbo(n-2);
+}
+//app
+#include"head.h"
+int main()
+{
+   int n,f;
+   printf("Enter a value for n:");
+   scanf("%d",&n);
+   f=fibbo(n);
+   printf("The %dth term of fibbonacci series is %d",n,f);
+   return 0;
+}
