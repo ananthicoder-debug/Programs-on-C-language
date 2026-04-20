9922b50606b20ea15ea5c485c9e677af0e1a5790
Anantha valli U <ananthigomathi202006@gmail.com>
2026-04-20 20:17:52 +0530

    Implement text file input and statistics program
    This program reads text input from the user, writes it to a file, and then counts the number of lines, words, and characters in that file.

---
+++ ex10/Functions in file.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+   FILE *fp;
+   char text[200];
+   int lines=0,words=0,c=0,i;
+   printf("Enter a text inside a file:");
+   scanf("%[^_]",text);
+   fp=fopen("text.txt","w");
+   if(fp==NULL)
+   {
+      printf("Error in opening the file\n");
+      return 0;
+   }
+   for(i=0;text[i]!='\0';i++)
+   {
+      fputc(text[i],fp);
+   }
+   fclose(fp);
+   fp=fopen("text.txt","r");
+   if(fp==NULL)
+   {
+      printf("Error in opening the file\n");
+      return 0;
+   }
+   char ch;
+   while((ch = fgetc(fp)) != EOF)
+   {
+      if(ch=='\n')
+      {
+         lines++;
+         words++;
+      }
+      else if(ch==' ')
+      {
+         words++;
+      }
+      c++;
+   }
+   printf("Number of lines:%d\n",lines);
+   printf("Number of words:%d\n",words);
+   printf("Number of characters:%d\n",c);
+   fclose(fp);
+   return 0;
+}
